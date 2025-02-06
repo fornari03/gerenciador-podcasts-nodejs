e69e9c88c5e8d3122215027ddeb6b3a0bb1ec448
fornari03 <221017023@aluno.unb.br>
2025-02-06 19:07:49 -0300

    refac: separa server e app para garantir o princípio de responsabilidade única

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,7 @@
 import * as http from "http";
-import {getListEpisodes, getFilterEpisodes} from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethods } from "./utils/http-methods";
+import { app } from "./app";
 
-const server = http.createServer( async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const [baseUrl, queryString] = req.url?.split("?") ?? [req.url, ""];
-
-    if (req.method === HttpMethods.GET && baseUrl === Routes.LIST) {
-        await getListEpisodes(req, res);
-    }
-
-    if (req.method === HttpMethods.GET && baseUrl === Routes.FILTER) {
-        await getFilterEpisodes(req, res);
-    }
-});
+const server = http.createServer(app)
 
 const port = process.env.PORT
 
